e3a0fcbeb23bac58ae161a9ce67b9c6c8a1dbe19
AlanKolmakov <kolm.a.s.1983@gmail.com>
2021-10-22 19:38:12 +0800

    Сформировать список квадратов чисел от 1 до 10 (с помощью метода append())

---
+++ py/HomeWork_15.py
@@ -0,0 +1,7 @@
+# Сформировать список квадратов чисел от 1 до 10 (с помощью метода append())
+
+list_of_squares = []
+for i in range(10):
+    i += 1
+    list_of_squares.append(i ** 2)
+print("Список квадратов = ", list_of_squares)
