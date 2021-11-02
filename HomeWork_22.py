b54bcdd3987af775e0558bb9d68e44304180e058
AlanKolmakov <kolm.a.s.1983@gmail.com>
2021-11-02 17:25:14 +0800

    Поиск заданного элемента в кортеже

---
+++ HomeWork_22.py
@@ -0,0 +1,8 @@
+# Поиск заданного элемента в кортеже
+
+a = ('ab', 'abcd', 'cde', 'abc', 'def')
+s = input(f'{a}\nВведите искомый элемент: ')
+print(f"{a}\ns = {s}")
+for i in a:
+    if s == i:
+        print('Yes')
