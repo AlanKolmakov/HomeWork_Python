7377fbc28bfd5e91bb20dc2cc774237875a5e7c2
AlanKolmakov <kolm.a.s.1983@gmail.com>
2021-11-02 17:28:14 +0800

    не туда добавил

--- HomeWork_22.py
+++
@@ -1,8 +0,0 @@
-# Поиск заданного элемента в кортеже
-
-a = ('ab', 'abcd', 'cde', 'abc', 'def')
-s = input(f'{a}\nВведите искомый элемент: ')
-print(f"{a}\ns = {s}")
-for i in a:
-    if s == i:
-        print('Yes')
