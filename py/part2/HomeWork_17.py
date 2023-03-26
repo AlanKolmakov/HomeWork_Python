ee97c955889caaa943d0b13615837a20b39824ca
AlanKolmakov <kolm.a.s.1983@gmail.com>
2023-03-27 02:52:11 +0800

    Приложение работы с фильмами, которое будет реализовывать паттерн MVC.

--- py/part2/HomeWork_17.py
+++ py/part2/HomeWork_17.py
@@ -10,4 +10,4 @@ def triangle(a, b, c):
 side1 = int(input("Сторона 1: "))
 side2 = int(input("Сторона 2: "))
 angle = int(input("Угол: "))
-print("Плащадь треугольника: ", triangle(side1, side2, angle))
+print("Площадь треугольника: ", triangle(side1, side2, angle))
